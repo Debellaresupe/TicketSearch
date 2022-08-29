4d0622a431f7ee21492315502549e6e321d536a1
Elnar Kilmukhametov <soulmiker@gmail.com>
2022-08-29 20:07:53 +0300

    add comparator

--- src/main/java/ru/netology/manager/TicketManager.java
+++ src/main/java/ru/netology/manager/TicketManager.java
@@ -1,5 +1,6 @@
 package ru.netology.manager;
 
+import ru.netology.comparator.TicketByFlightTimeComparator;
 import ru.netology.repository.TicketRepository;
 import ru.netology.ticket.Ticket;
 
@@ -7,6 +8,8 @@ import java.util.Arrays;
 
 public class TicketManager {
     private TicketRepository repo;
+
+    TicketByFlightTimeComparator flightTimeComparator = new TicketByFlightTimeComparator();
 
     public TicketManager(TicketRepository repo) {
         this.repo = repo;
@@ -37,6 +40,22 @@ public class TicketManager {
             }
         }
         Arrays.sort(result);
+        return result;
+    }
+
+    public Ticket[] searchAndSortByFlightTime(String departureAirport, String arrivalAirport) {
+        Ticket[] result = new Ticket[0];
+        for (Ticket ticket : repo.findAll()) {
+            if (matches(ticket, departureAirport, arrivalAirport)) {
+                Ticket[] tmp = new Ticket[result.length + 1];
+                for (int i = 0; i < result.length; i++) {
+                    tmp[i] = result[i];
+                }
+                tmp[tmp.length - 1] = ticket;
+                result = tmp;
+            }
+        }
+        Arrays.sort(result, flightTimeComparator);
         return result;
     }
 }
